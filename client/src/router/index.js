c215e47fb5a6d9f2ce7118153731e8a1ebb5c618
MikroTik2 <111369356+MikroTik2@users.noreply.github.com>
2023-11-30 16:43:10 +0200

    modified router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Index from '@/views/Index.vue';
 import ProjectDetail from '@/views/Project.vue';
 
 const router = createRouter({
-     history: createWebHashHistory(),
+     history: createWebHistory(),
 
      routes: [
           { path: "/", name: "Index", component: Index },
