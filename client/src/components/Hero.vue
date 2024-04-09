ec38462c848517459d917098b23489d52b3e63c5
gf2004 <dotsenk20034ua@gmail.com>
2024-04-09 18:21:29 +0300

    download pdf file v4

--- client/src/components/Hero.vue
+++ client/src/components/Hero.vue
@@ -14,15 +14,17 @@
                                 
                                         <div class="hero-content-left__btn">
                                                 <Button type="link" href="#project" :color="colorLink || 'white'">My Project</Button>
-                                                <Button type="link" href="/public/pdf/Доценко Артур Юрийович.pdf">
-                                                        Download resume
+                                                <a download="/public/pdf/Доценко Артур Юрийович.pdf">
+                                                        <Button type="link">
+                                                                Download resume
 
-                                                        <div class="hero-content-left__btn-size">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                                                </svg>
-                                                        </div>
-                                                </Button>
+                                                                <div class="hero-content-left__btn-size">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                                                        </svg>
+                                                                </div>
+                                                        </Button>
+                                                </a>
                                         </div>
                                 
                                 </div>
