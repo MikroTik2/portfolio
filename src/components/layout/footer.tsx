a4fcd849e8887a8b7dc1dfa845fe3b56dddabd8f
Artur <111369356+MikroTik2@users.noreply.github.com>
2025-07-26 01:48:14 +0300

    fix: remove link in the footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -72,20 +72,14 @@ export function Footer() {
 	return (
 		<footer className='mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800'>
 			<div className='flex items-center justify-between'>
-				<a
-					href='https://github.com/ibelick/nim'
-					target='_blank'
-				>
-					<TextLoop className='text-xs text-zinc-500'>
-						<span>
-							© {new Date().getFullYear()} Доценко.
-						</span>
-						<span>
-							Створено з використанням
-							Motion-Primitives.
-						</span>
-					</TextLoop>
-				</a>
+				<TextLoop className='text-xs text-zinc-500'>
+					<span>
+						© {new Date().getFullYear()} Доценко.
+					</span>
+					<span>
+						Створено з використанням Motion-Primitives.
+					</span>
+				</TextLoop>
 				<div className='text-xs text-zinc-400'>
 					<ThemeSwitch />
 				</div>
